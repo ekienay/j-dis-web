f86e8b1356fcde5eb29a28d1beb9960cb531dae1
ekienay <ekienay@gmail.com>
2021-05-11 23:17:02 +0300

    html/css/js

---
+++ assets/JavaScript/Script.js
@@ -0,0 +1,12 @@
+
+
+function openbox(con) {
+	
+	display = document.getElementById('con').style.display;
+
+	if(display == "block") {
+		document.getElementById('con').style.display = "none";
+	} else {
+		document.getElementById('con').style.display = "block";
+	}
+}
